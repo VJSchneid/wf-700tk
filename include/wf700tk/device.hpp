5dcb078d222342c6e79437507ba17eda469db430
Viktor Schneider <info@vjs.io>
2020-10-10 12:16:46 +0200

    start implementation of device. not complete yet!

--- include/wf700tk/device.hpp
+++ include/wf700tk/device.hpp
@@ -3,15 +3,20 @@
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/serial_port.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <functional>
 #include <system_error>
 
+#include <wf700tk/response_parser.hpp>
 #include <wf700tk/ticket_channel.hpp>
 
 namespace wf700tk {
 
 namespace net = boost::asio;
 
+class request;
+
+// TODO: error handling
 class device {
 public:
     using error_code = boost::system::error_code;
@@ -20,26 +25,65 @@ public:
 
     device(net::serial_port &&port);
 
-    void start(const std::function<void(const error_code &)> &func);
+    inline void start() {
+        start_poll();
+        start_read();
+    }
 
-    bool enable_input() const;
-    void enable_input(bool);
+    inline void polling_duration(const clock::duration &v) {
+        polling_duration_ = v;
+    }
 
-    void polling_duration(const clock::duration &v);
-    clock::duration polling_duration() const;
+    inline clock::duration polling_duration() const {
+        return polling_duration_;
+    }
 
-    void output_timeout(const clock::duration &v);
-    clock::duration output_timeout() const;
-
-    void input_callback(std::function<void()>);
+    void input_callback(const std::function<void(const error_code &ec,
+                                                 credit_channel)> &callback);
 
     void output(ticket_channel channel, unsigned int amount,
                 const output_handler &handler);
 
 private:
-    void read();
+    void start_poll();
+
+    void start_read();
+    void handle_read(std::size_t len);
+    void handle_poll_response(const response &resp);
+
+    void poll();
+
+    void prepare_poll(request &req);
 
     net::serial_port port_;
+
+    net::steady_timer poll_timer_;
+    clock::duration polling_duration_ = std::chrono::milliseconds(200);
+
+    std::vector<unsigned char> request_buffer_;
+    std::vector<unsigned char> response_buffer_{32};
+    response_parser response_reader_;
+
+    struct output_state {
+        bool active = false;
+        int to_dispense = 0;
+        std::function<void(const error_code &ec, unsigned int dispensed)>
+            callback;
+
+        void inform(unsigned char tickets_dispensed);
+        unsigned char next_dispense_amount() const;
+        void prepare_for_sending();
+        void reset();
+        bool timed_out() const;
+
+    private:
+        unsigned char last_dispense_state = 0x00;
+        int timeout_counter = 0;
+    };
+
+    std::array<output_state, 4> ticket_output_;
+
+    std::function<void(const error_code &, credit_channel)> input_callback_;
 };
 
 } // namespace wf700tk
--- src/device.cpp
+++ src/device.cpp
@@ -1,11 +1,141 @@
 #include <wf700tk/device.hpp>
 
+#include <algorithm>
+
+#include <boost/asio/buffer.hpp>
 #include <boost/asio/read_until.hpp>
+#include <boost/asio/write.hpp>
+
+#include <wf700tk/request_writer.hpp>
 
 namespace wf700tk {
 
-device::device(net::serial_port &&port) : port_(std::move(port)) {}
+device::device(net::serial_port &&port)
+    : port_(std::move(port)), poll_timer_(port_.get_executor()),
+      request_buffer_(request_writer{}.length()) {}
 
-void device::read() {}
+void device::input_callback(
+    const std::function<void(const device::error_code &, credit_channel)>
+        &callback) {
+    if (input_callback_) {
+        input_callback_(error_code(net::error::operation_aborted),
+                        credit_channel::none);
+    }
+    input_callback_ = callback;
+}
+
+void device::start_poll() {
+    poll_timer_.expires_after(polling_duration_);
+    poll_timer_.async_wait([this](const error_code &ec) {
+        if (!ec) {
+            poll();
+        }
+    });
+}
+
+void device::start_read() {
+    port_.async_read_some(net::buffer(response_buffer_),
+                          [this](const error_code &ec, std::size_t len) {
+                              if (!ec) {
+                                  handle_read(len);
+                              }
+                          });
+}
+
+void device::handle_read(std::size_t len) {
+    response_reader_.put(net::buffer(response_buffer_.data(), len));
+    if (response_reader_.success()) {
+        handle_poll_response(response_reader_.message());
+    }
+}
+
+void device::handle_poll_response(const response &resp) {
+    if (resp.accepted_credit_) {
+        input_callback_(error_code(), resp.credit_channel_);
+    }
+
+    if (auto i = ticket_channel_index(resp.ticket_channel_)) {
+        auto &channel = ticket_output_[*i];
+        if (channel.active) {
+            channel.inform(resp.tickets_already_dispensed_);
+        }
+    }
+}
+
+void device::poll() {
+    request_writer req_w;
+    prepare_poll(req_w.msg());
+    auto buf = net::buffer(request_buffer_);
+    req_w.write(buf);
+
+    net::async_write(
+        port_, buf,
+        [this](const error_code &ec, std::size_t /*bytes_written*/) {
+            if (!ec) {
+                start_poll();
+            }
+        });
+}
+
+void device::prepare_poll(request &req) {
+    for (unsigned int i = 0; i < ticket_output_.size(); ++i) {
+        // TODO: check wether this works. This should stop running outputs that
+        // timed out
+        auto &output = ticket_output_[i];
+        if (output.timed_out()) {
+            req.ticket_channel_ = *make_ticket_channel_from_index(i);
+            req.tickets_to_dispense(0);
+            output.callback(error_code(net::error::timed_out),
+                            output.to_dispense);
+            output.reset();
+            return;
+        }
+    }
+
+    for (unsigned int i = 0; i < ticket_output_.size(); ++i) {
+        auto &output = ticket_output_[i];
+        if (!output.active && output.to_dispense > 0) {
+            req.ticket_channel_ = *make_ticket_channel_from_index(i);
+            req.tickets_to_dispense(output.next_dispense_amount());
+            output.prepare_for_sending();
+            return;
+        }
+    }
+}
+
+void device::output_state::inform(unsigned char tickets_dispensed) {
+    if (last_dispense_state < tickets_dispensed) {
+        timeout_counter = 0;
+
+        to_dispense =
+            std::max(0, to_dispense - last_dispense_state - tickets_dispensed);
+
+        if (to_dispense == 0) {
+            callback(error_code(), 0);
+            reset();
+        }
+    } else {
+        ++timeout_counter;
+    }
+    last_dispense_state = tickets_dispensed;
+}
+
+unsigned char device::output_state::next_dispense_amount() const {
+    return std::min(static_cast<unsigned char>(to_dispense),
+                    static_cast<unsigned char>(0x0f));
+}
+
+void device::output_state::prepare_for_sending() {
+    last_dispense_state = 0x00;
+    active = true;
+}
+
+void device::output_state::reset() {
+    active = false;
+    to_dispense = 0;
+    callback = decltype(callback)();
+}
+
+bool device::output_state::timed_out() const { return timeout_counter > 5; }
 
 } // namespace wf700tk
